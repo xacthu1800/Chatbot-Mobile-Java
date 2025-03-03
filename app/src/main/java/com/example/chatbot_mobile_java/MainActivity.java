459576253a19815f298e5000807ec8e6dd7a6f43
NguyenVanBin_2151012001 <bin2003mail@gmail.com>
2025-03-03 17:16:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chatbot_mobile_java/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.chatbot_mobile_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_constraint_layout);
+    }
+}
