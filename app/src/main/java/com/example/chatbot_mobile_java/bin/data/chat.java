e2827f76d2a6a1882b8a5520711e7f38a870f468
NguyenVanBin_2151012001 <bin2003mail@gmail.com>
2025-03-03 22:31:55 +0700

    chore: config gradle and finished history-chat activity

--- app/src/main/java/com/example/chatbot_mobile_java/bin/data/chat.java
+++ app/src/main/java/com/example/chatbot_mobile_java/bin/data/chat.java
@@ -1,7 +1,17 @@
 package com.example.chatbot_mobile_java.bin.data;
 
 public class chat {
-//    data class chat(
-//            val chatValue: String
-//    )
+    public String getChatText() {
+        return chatText;
+    }
+
+    public chat(String chatText) {
+        this.chatText = chatText;
+    }
+
+    public void setChatText(String chatText) {
+        this.chatText = chatText;
+    }
+
+    private String  chatText ;
 }
--- app/src/main/java/com/example/chatbot_mobile_java/bin/data/listChat.java
+++ app/src/main/java/com/example/chatbot_mobile_java/bin/data/listChat.java
@@ -1,8 +1,28 @@
 package com.example.chatbot_mobile_java.bin.data;
 
-public class listChat {
-//    data class listChat(
-//            val listChat: MutableList<chat>
-//    )
+import java.util.ArrayList;
+import java.util.List;
 
+public class listChat {
+    private List<chat> listChat;
+
+    public listChat(List<chat> listChat) {
+        this.listChat = new ArrayList<>();
+    }
+
+    public List<chat> getListChat() {
+        return listChat;
+    }
+
+    public void setListChat(List<chat> listChat) {
+        this.listChat = listChat;
+    }
+
+    public void addItemToListChat(chat newChat){
+        this.listChat.add(newChat);
+    }
+
+    public chat getItematIndexInListChat(int index){
+        return this.listChat.get(index);
+    }
 }
